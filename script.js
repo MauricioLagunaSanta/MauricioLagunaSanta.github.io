5bb0c738f5da802abc63bfdc098b9bff49d326e7
MauricioLagunaSanta <mauriciolagunasanta@gmail.com>
2026-03-12 16:50:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function mostrarMensaje() {
-    alert("Gracias por tu interés en Style Models. ¡Pronto nos pondremos en contacto contigo!");
-}
-
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Formulario enviado correctamente. ¡Gracias por contactarnos!");
-});
+console.log("Style Models website loaded");
